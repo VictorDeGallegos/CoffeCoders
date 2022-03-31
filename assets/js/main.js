bc29dd8783f298fe52383fcef2a85d351e0a7a17
Victor Gallegos <316160456@ciencias.unam.mx>
2022-03-30 19:13:36 -0600

    Header & Nav Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,30 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-
+  // Validar que existen variables
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      // Añadimos el show-menu class a la etiqueta div con el nav__menu class
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
-
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // Cuando hacemos clic en cada nav__link, eliminamos el show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SHOW SCROLL UP ===============*/
-
 
 /*=============== DARK LIGHT THEME ===============*/
